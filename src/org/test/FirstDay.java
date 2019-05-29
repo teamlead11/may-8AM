4fbfdde84e000778403ec45ccc7bc14374812fcc
teamlead11 <karthirajan0706@gmail.com>
2019-05-29 08:43:17 +0530

    initial one to git

---
+++ src/org/test/FirstDay.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class FirstDay {
+	
+static int x=30;
+	
+	static {System.out.println("static block");}
+
+}
---
+++ src/org/test/Welcome.java
@@ -0,0 +1,20 @@
+package org.test;
+
+
+
+public class Welcome {
+public static void main(String[] args) {
+		
+		int a=0;
+		
+		for (int i = 0; i <2; i++) {
+			for (int j = 0; j <4; j++) {
+				a=i;
+				
+			}
+			System.out.println(j);
+			
+		}
+	}
+
+}
